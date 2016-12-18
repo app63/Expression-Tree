674d6e55a3f242e897aaecd56eaf475f802b5348
app63 <app63@njit.edu>
2016-12-18 12:53:05 -0500

    Final commit

--- Program3.sh
+++ Program3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-javac Main.java
+javac Prefixer.java
 javac PrefixCalc.java
 
-java Main
-echo -e "Enter the reverse of the above expression"
+java Prefixer -r test.txt
+echo -e "Please enter the above expression below with brackets"
 java PrefixCalc
 
